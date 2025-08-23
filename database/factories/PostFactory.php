b2afae411a634f292d2cb9ac9c8d4af942641334
Anneli Peep <anneli.peep@gmail.com>
2025-08-23 15:06:03 +0300

    first commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Database\Seeders\PostSeeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraph(6, true),
+        ];
+
+
+    }
+}
